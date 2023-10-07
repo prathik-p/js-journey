04eded3f86cc42fee9fa687e59cd8cca5fa76114
bigbarin69 <prathik.prejith@gmail.com>
2023-10-07 09:54:50 +0530

    react js starter

---
+++ js-starter/src/App.tsx
@@ -0,0 +1,13 @@
+import ListGroup from "./components/ListGroup";
+import NavBar from "./components/NavBar";
+
+function App() {
+  return (
+    <div>
+      <NavBar />
+      <ListGroup />
+    </div>
+  );
+}
+
+export default App;
